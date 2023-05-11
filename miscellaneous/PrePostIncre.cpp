b88b901f2c4a041c62307b7e7f9fad8f8ac29cd8
itisaraf <itisaraf@gmail.com>
2023-05-11 16:43:12 +0600

    updated

--- miscellaneous/PrePostIncre.cpp
+++ miscellaneous/PrePostIncre.cpp
@@ -6,11 +6,47 @@ int main()
 
     int a, b;
 
-    a=4;
-    b= a++ + ++a ;
-     cout <<  b <<" " << a << endl;
-    a=4;
+    a=1;
 
-    b=  ++a + a++  ;
-    cout <<  b <<" " << a << endl;
+    b= a++ + a++ ;
+    cout <<  b <<" " << a << endl;  // 3 3
+
+    a=1;
+
+    b=  ++a + ++a  ;
+    cout <<  b <<" " << a << endl; // 6 3
+
+    a=1;
+
+    b=  ++a + ++a + ++a  ;
+    cout <<  b <<" " << a << endl; // 10 4
+
+    a=1;
+
+    b=  ++a + ++a  + a++;
+    cout <<  b <<" " << a << endl; // 9 4
+
+    a=1;
+
+    b=  ++a + a++ + ++a  ;
+    cout <<  b <<" " << a << endl; // 9 4
+
+    a=1;
+
+    b=  ++a + a++ + a++;
+    cout <<  b <<" " << a << endl; // 8 4
+
+    a=1;
+
+    b=  a++ + a++ + ++a  ;
+    cout <<  b <<" " << a << endl; // 7 4
+
+
+    a=1;
+
+    b=  a++ + a++ + a++  ;
+    cout <<  b <<" " << a << endl; // 6 4
+
+
 }
+
